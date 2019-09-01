5ebaa084f68618adcfbb4f129bdeac74ea1eb6fa
mahib uddin <mahibuddin6@gmail.com>
2019-09-01 19:36:32 +0600

    file change many things

--- obj-lesson.js
+++ obj-lesson.js
@@ -2,12 +2,12 @@
 // Basic Object Creation
 
 var book = {
-    name: 'functional javascript',
+    name: 'functional javascript', // : use korar karon holo obj er vitore / {} er vitore... er baire hole = use kortam...
     authore: 'Micbel Fogus',
     publisher: 'O\'Reilly',
     page: 250,
 
-    print:function(){
+    print:function(){ // this is called method not function
         console.log(this.name, this.authore);
         
     },
@@ -15,23 +15,26 @@ var book = {
     
 }
 
-book.publisheryear = 2010;
+book.print();  // ekhane method tike call kora holo...
+
+// javascript er jekono object ke baire theke extend korte pari... exmp : below...
+book.publisheryear = 2010;  // = use korar karon holo object er baire...
 
 console.log('Published Year = ' + book.publisheryear);
 
 book['price'] = 45;
 
+// amra 2 ta notation use korte pari, 1. dot notation  2. brakets notation... exmp: below...
 console.log('Book Price = ' + book.price);
+console.log('Book Price = ' + book['price']);
 
-
+// kibave properties guloke travers korte pari...
+    // for in loops er maddhome travers korbo ...
     for(var props in book){
-        console.log(props);
-        console.log(props + ' = ' + book[props]);
+        console.log(props); // object key show korte...
+        console.log(props + ' = ' + book[props]); // object value show korte...
         
     }
 
 
-
-    
-// Class Object Creation
 
